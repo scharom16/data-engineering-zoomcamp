b5653919ca65fc2d9abd5099ca72b7582a70021a
Jerome <oss@scharom.de>
2022-02-13 18:35:53 +0000

    Implement live coding workshop

--- week_3_data_warehouse/airflow/dags/gcs_to_bq_dag.py
+++ week_3_data_warehouse/airflow/dags/gcs_to_bq_dag.py
@@ -59,13 +59,20 @@ with DAG(
                 },
             },
         )
-
-        CREATE_BQ_TBL_QUERY = (
-            f"CREATE OR REPLACE TABLE {BIGQUERY_DATASET}.{colour}_{DATASET} \
-            PARTITION BY DATE({ds_col}) \
-            AS \
-            SELECT * FROM {BIGQUERY_DATASET}.{colour}_{DATASET}_external_table;"
-        )
+        if colour == "yellow":
+            CREATE_BQ_TBL_QUERY = (
+                f"CREATE OR REPLACE TABLE {BIGQUERY_DATASET}.{colour}_{DATASET} \
+                PARTITION BY DATE({ds_col}) \
+                AS \
+                SELECT * FROM {BIGQUERY_DATASET}.{colour}_{DATASET}_external_table;"
+            )
+        else:
+            CREATE_BQ_TBL_QUERY = (
+                f"CREATE OR REPLACE TABLE {BIGQUERY_DATASET}.{colour}_{DATASET} \
+                PARTITION BY DATE({ds_col}) \
+                AS \
+                SELECT * EXCEPT (ehail_fee) FROM {BIGQUERY_DATASET}.{colour}_{DATASET}_external_table;"
+            )
 
         # Create a partitioned table from external table
         bq_create_partitioned_table_job = BigQueryInsertJobOperator(
