b93bca0414d22fc94a6c4c1ab1365d529f4ea27e
Jerome <oss@scharom.de>
2022-02-13 13:08:44 +0000

    Refactor paremetrized dag

--- week_2_data_ingestion/airflow/dags/data_ingestion_gcs_dag.py
+++ week_2_data_ingestion/airflow/dags/data_ingestion_gcs_dag.py
@@ -15,15 +15,22 @@ PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
 AIRFLOW_HOME = f'{os.environ.get("AIRFLOW_HOME", "/opt/airflow/")}/'
-URL_PREFIX = 'https://nyc-tlc.s3.amazonaws.com/trip+data/'
-FILE_NAME = 'fhv_tripdata_{{execution_date.strftime(\'%Y-%m\')}}'
-URL_TEMPLATE = URL_PREFIX + f'{FILE_NAME}.csv'
-OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + f'{FILE_NAME}'
 
-URL_PREFIX_ZONES = 'https://s3.amazonaws.com/nyc-tlc/misc/'
-ZONES_NAME = 'taxi+_zone_lookup'
-ZONES_URL = f'{URL_PREFIX_ZONES}{ZONES_NAME}.csv'
-ZONES_OUTPUT = AIRFLOW_HOME + ZONES_NAME
+YELLOW_URL_PREFIX = 'https://s3.amazonaws.com/nyc-tlc/trip+data/'
+YELLOW_FILE_NAME_TEMPLATE = 'zones_tripdata_{{execution_date.strftime(\'%Y-%m\')}}'
+YELLOW_URL_TEMPLATE = YELLOW_URL_PREFIX + f'{YELLOW_FILE_NAME_TEMPLATE}.csv'
+YELLOW_OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + f'{YELLOW_FILE_NAME_TEMPLATE}'
+YELLOW_GCS_PATH_TEMPLATE = "raw/zones_tripdata/{{execution_date.strftime(\'%Y\')}}/" +f"{YELLOW_FILE_NAME_TEMPLATE}.parquet"
+
+FHV_URL_PREFIX = 'https://nyc-tlc.s3.amazonaws.com/trip+data/'
+FHV_FILE_NAME_TEMPLATE = 'fhv_tripdata_{{execution_date.strftime(\'%Y-%m\')}}'
+FHV_URL_TEMPLATE = FHV_URL_PREFIX + f'{FHV_FILE_NAME_TEMPLATE}.csv'
+FHV_OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + f'{FHV_FILE_NAME_TEMPLATE}'
+FHV_GCS_PATH_TEMPLATE = "raw/fhv_tripdata/{{execution_date.strftime(\'%Y\')}}/" +f"{FHV_FILE_NAME_TEMPLATE}.parquet"
+
+ZONES_URL_TEMPLATE = 'https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv'
+ZONES_OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + 'taxi_zone_lookup'
+ZONES_GCS_PATH_TEMPLATE = "raw/taxi_zones/taxi_zone_lookup.parquet"
 
 def format_to_parquet(src_file):
     if not src_file.endswith('.csv'):
@@ -54,6 +61,44 @@ def upload_to_gcs(bucket, object_name, local_file):
     blob = bucket.blob(object_name)
     blob.upload_from_filename(local_file)
 
+def download_parquetized_upload_data(
+    dag,
+    url_template,
+    local_path_template,
+    gcs_path_template
+):
+    with dag:
+
+        download_dataset_task = BashOperator(
+            task_id="download_dataset_task",
+            bash_command=f"curl -sSLf {url_template} > {local_path_template}.csv"
+        )
+
+        format_to_parquet_task = PythonOperator(
+            task_id="format_to_parquet_task",
+            python_callable=format_to_parquet,
+            op_kwargs={
+                "src_file": f"{local_path_template}.csv",
+            },
+        )
+
+        local_to_gcs_task = PythonOperator(
+            task_id="local_to_gcs_task",
+            python_callable=upload_to_gcs,
+            op_kwargs={
+                "bucket": BUCKET,
+                "object_name": gcs_path_template,
+                "local_file": f"{local_path_template}.parquet",
+            },
+        )
+
+        delete_files_task = BashOperator(
+            task_id="detete_files_task",
+            bash_command=f"rm {local_path_template}.parquet {local_path_template}.csv"
+        )
+
+        download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> delete_files_task
+
 
 default_args = {
     "owner": "airflow",
@@ -61,93 +106,17 @@ default_args = {
     "depends_on_past": False,
     "retries": 1,
 }
-
-# NOTE: DAG declaration - using a Context Manager (an implicit way)
-with DAG(
-    dag_id="data_ingestion_gcs_dag_v01",
-    schedule_interval="@monthly",
-    default_args=default_args,
-    catchup=True,
-    max_active_runs=3,
-    tags=['dtc-de'],
-) as dag:
-
-    download_dataset_task = BashOperator(
-        task_id="download_dataset_task",
-        bash_command=f"curl -sSf {URL_TEMPLATE} > {OUTPUT_FILE_TEMPLATE}.csv"
-    )
-
-    format_to_parquet_task = PythonOperator(
-        task_id="format_to_parquet_task",
-        python_callable=format_to_parquet,
-        op_kwargs={
-            "src_file": f"{OUTPUT_FILE_TEMPLATE}.csv",
-        },
-    )
-
-    # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
-    local_to_gcs_task = PythonOperator(
-        task_id="local_to_gcs_task",
-        python_callable=upload_to_gcs,
-        op_kwargs={
-            "bucket": BUCKET,
-            "object_name": f"raw/{FILE_NAME}.parquet",
-            "local_file": f"{OUTPUT_FILE_TEMPLATE}.parquet",
-        },
-    )
-    delete_files = BashOperator(
-        task_id="detete_files",
-        bash_command=f"rm {OUTPUT_FILE_TEMPLATE}.parquet {OUTPUT_FILE_TEMPLATE}.csv"
-    )
-
-    # bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-    #     task_id="bigquery_external_table_task",
-    #     table_resource={
-    #         "tableReference": {
-    #             "projectId": PROJECT_ID,
-    #             "datasetId": BIGQUERY_DATASET,
-    #             "tableId": "external_table",
-    #         },
-    #         "externalDataConfiguration": {
-    #             "sourceFormat": "PARQUET",
-    #             "sourceUris": [f"gs://{BUCKET}/raw/{parquet_file}"],
-    #         },
-    #     },
-    # )
-
-    download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> delete_files
-
-
-with DAG(
-    dag_id="data_ingestion_zones_v01",
+dag_zones = DAG(
+    dag_id="zones_data_ingestion_gcs_dag_v01",
     schedule_interval="@once",
     default_args=default_args,
-    catchup=False,
-    max_active_runs=1,
+    catchup=True,
+    max_active_runs=4,
     tags=['dtc-de'],
-) as dag:
+)
 
-    download_zones_task = BashOperator(
-        task_id="download_zones_task",
-        bash_command=f"curl -sSf {ZONES_URL} > {ZONES_OUTPUT}.parquet"
-    )
-
-    parquetize_zones_task = PythonOperator(
-        task_id="parquetize_zones_task",
-        python_callable=format_to_parquet,
-        op_kwargs={
-            "src_file": f"{ZONES_OUTPUT}.parquet",
-        }
-    )
-    
-    local_to_gcs_task = PythonOperator(
-        task_id="local_to_gcs_task",
-        python_callable=upload_to_gcs,
-        op_kwargs={
-            "bucket": BUCKET,
-            "object_name": f"raw/{ZONES_NAME}.parqeut",
-            "local_file": f"{ZONES_OUTPUT}.parquet",
-        }
-    )
-
-    download_zones_task >> parquetize_zones_task >> local_to_gcs_task
+download_parquetized_upload_data(dag_zones,
+    ZONES_URL_TEMPLATE,
+    ZONES_OUTPUT_FILE_TEMPLATE,
+    ZONES_GCS_PATH_TEMPLATE
+)
